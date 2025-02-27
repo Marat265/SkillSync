c39ee10aadb5c9d0119c7d145280b555b356d5d6
Marat <maratagajanov@mail.ru>
2025-02-27 20:09:01 +0400

    Adding Services folder and start implementing ISessionRepository

---
+++ backend/Services/SessionRepository.cs
@@ -0,0 +1,68 @@
+﻿using Portfolio.Models;
+using Skillsync.Repositories;
+
+namespace Skillsync.Services
+{
+    public class SessionRepository : ISessionRepository
+    {
+        public Task AddSessionAsync(Session session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddSessionRegistrationAsync(SessionRegistration sessionRegistration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSessionAsync(Session session)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSessionRegistrationAsync(SessionRegistration sessionRegistration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<SessionRegistration>> GetAllSessionRegistrationsWithMentorsAsync(string studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Session>> GetAllSessionsWithMentorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Session> GetMentorSessionByIdAsync(int sessionId, string mentorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Session>> GetMentorSessionsByIdAsync(string mentorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SessionRegistration> GetSessionRegistrationAsync(string studentId, int sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Session> GetSessionWithMentorByIdAsync(int sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Session> GetSessionWithRegistrationsByIdAsync(int sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
