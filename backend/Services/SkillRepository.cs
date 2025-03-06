018bb10b843224d5d119fdd49f2e08877194214b
Marat <maratagajanov@mail.ru>
2025-03-06 12:57:56 +0400

    Used SkillRepository in MentorController

--- backend/Services/SkillRepository.cs
+++ backend/Services/SkillRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace Skillsync.Services
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Data;
+using Portfolio.Models;
+using Skillsync.Repositories;
+
+namespace Skillsync.Services
 {
-    public class SkillRepository
+    public class SkillRepository : ISkillRepository
     {
+        private readonly ApplicationDbContext _context;
+        public SkillRepository(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        public async Task<Skills> FindSkillByNameAsync(string skillName)
+        {
+            return await _context.Skills.FirstOrDefaultAsync(s => s.Name == skillName);
+        }
+        public async Task<MentorSkills> FindMentorSkillByIdAsync(string mentorID, int skillID)
+        {
+            return await _context.MentorSkills.FirstOrDefaultAsync(ms => ms.MentorId == mentorID
+            && ms.SkillId == skillID);
+        }
+        public void AddMentorSkill(MentorSkills skill)
+        {
+            _context.MentorSkills.Add(skill);
+        }
+        public void RemoveMentorSkill(MentorSkills skill)
+        {
+            _context.MentorSkills.Remove(skill);
+        }
+
+        public void AddSkill(Skills skill)
+        {
+            _context.Skills.Add(skill);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
     }
 }
