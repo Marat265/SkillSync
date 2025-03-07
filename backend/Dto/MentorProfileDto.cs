abb7f4c643add0b0568e997c69d0ac71bea80ca2
Marat <maratagajanov@mail.ru>
2025-03-07 16:26:50 +0400

    displaying photos in the mentor's profile

--- backend/Dto/MentorProfileDto.cs
+++ backend/Dto/MentorProfileDto.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Image { get; set; }
         public DateTime CreatedAt { get; set; }
         public List<string> Skills { get; set; } = new();
         public List<string> Reviews { get; set; } = new();
